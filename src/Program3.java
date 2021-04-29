72cdef40ca306024134749fa6dffc8563bf40589
Eralp Orkun <eaorkun@gmail.com>
2021-04-29 17:03:18 -0500

    Initial commit

---
+++ src/Program3.java
@@ -0,0 +1,47 @@
+/*
+ * Name: Eralp Orkun
+ * EID: eao789
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * Your solution goes in this class.
+ * 
+ * Please do not modify the other files we have provided for you, as we will use
+ * our own versions of those files when grading your project. You are
+ * responsible for ensuring that your solution works with the original version
+ * of all the other files we have provided for you.
+ * 
+ * That said, please feel free to add additional files and classes to your
+ * solution, as you see fit. We will use ALL of your additional files when
+ * grading your solution.
+ */
+public class Program3 extends AbstractProgram3 {
+
+    /**
+     * Determines the solution of the optimal response time for the given input TownPlan. Study the
+     * project description to understand the variables which represent the input to your solution.
+     *
+     * @return Updated TownPlan town with the "responseTime" field set to the optimal response time
+     */
+    @Override
+    public TownPlan findOptimalResponseTime(TownPlan town) {
+        /* TODO implement this function */
+        return town;
+    }
+
+    /**
+     * Determines the solution of the set of police station positions that optimize response time for the given input TownPlan. Study the
+     * project description to understand the variables which represent the input to your solution.
+     *
+     * @return Updated TownPlan town with the "policeStationPositions" field set to the optimal police station positions
+     */
+    @Override
+    public TownPlan findOptimalPoliceStationPositions(TownPlan town) {
+        /* TODO implement this function */
+        return town;
+    }
+}
